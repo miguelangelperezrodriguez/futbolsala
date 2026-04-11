50764b81035ffee83f9369505d0ec9a06a0dbe09
Usuario <Usuario@PC-Libelula>
2026-04-11 10:57:54 +0200

    texto pagina

--- src/main/java/com/fmanantial/futbolsala/model/Equipo.java
+++ src/main/java/com/fmanantial/futbolsala/model/Equipo.java
@@ -1,7 +1,33 @@
 
 package com.fmanantial.futbolsala.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Table(name="equipos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Equipo {
+
+    // Clave Primaria
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    
+    private String nombre;
+    private String localidad;
+    
+    
+    
     
 }
--- src/main/java/com/fmanantial/futbolsala/model/Jugador.java
+++ src/main/java/com/fmanantial/futbolsala/model/Jugador.java
@@ -6,10 +6,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-
 @Table(name="jugadores")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Jugador {
     
     // Clave Primaria
@@ -23,12 +30,6 @@ public class Jugador {
     private String demarcacion;
     private Integer nrogoles;
     private Integer valoracion;
-    
-    
-    public Jugador()
-    {
-        
-    }
     
    
 }
